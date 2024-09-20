539d61513f1954c8fcd34e3decc80c7c8a5e8200
AntonioCarrenoF <antonio234@icloud.com>
2024-09-19 20:20:54 -0600

    Administrador Toques
    Se agregó la lógica del administrador de toques

--- TowerDefenseGit/Assets/_Scripts/AdministradorToques.cs
+++ TowerDefenseGit/Assets/_Scripts/AdministradorToques.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 public class AdministradorToques : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputActionAsset inputs;
+
+    private InputAction toque;
+    private InputAction posicionToque;
+    private Camera mainCam;
+
+    public delegate void PlataformaTocada(GameObject plataforma);
+    public event PlataformaTocada EnPlataformaTocada;
+
+    private void OnEnable()
     {
+        TouchSimulation.Enable();
+        inputs.Enable();
+        toque = inputs.FindAction("Toque");
+        posicionToque = inputs.FindAction("PosicionToque");
+        toque.performed += Toque;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        inputs.Disable();
+        TouchSimulation.Disable();
+        toque.performed -= Toque;
         
+    }
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    private void Toque(InputAction.CallbackContext obj)
+    {
+        Vector2 poseToque2D = posicionToque.ReadValue<Vector2>();
+        Vector3 poseToque3D = new Vector3(poseToque2D.x, poseToque2D.y, mainCam.farClipPlane);
+        Ray rayoPantalla = mainCam.ScreenPointToRay(poseToque3D);
+        Debug.Log($"La pantalla fue tocada en la posicion: {poseToque2D}");
+        RaycastHit hit;
+        if(Physics.Raycast(rayoPantalla, out hit, Mathf.Infinity))
+        {
+
+        }
+;      
     }
 }
